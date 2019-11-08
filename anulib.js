062436d84f70ca8a31ef707f2852e32460bbc348
admin <nguyenhoangthang11051@gmail.com>
2019-11-08 23:29:20 +0700

    Done

---
+++ anulib.js
@@ -0,0 +1,13 @@
+"use strict";
+function ff_extractDomain(url) {
+    var domain;
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+    domain = domain.split(':')[0];
+
+    return domain;
+}
---
+++ common.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function checkCapcha(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
